f18d2e0f973ef2aee315f795c6afe32063641014
pratibha-biradar <pratibha.m.biradar@gmail.com>
2025-02-12 08:41:51 +0530

    js01.variable and dataTypes Introduction
    js01.variable and dataTypes Introduction

---
+++ 01-variable.js
@@ -0,0 +1,11 @@
+var age; //variable declaration
+console.log(age); //undefined
+age="22";           //variable intialization
+console.log(age);
+console.log("AGE",age);
+
+var presentCity="Pune"; //variable declaration and intialization in same line
+console.log("My Present City:",presentCity);
+
+
+
